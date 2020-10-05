c0dcafe4dfdf1ff7bf9e9873b131278c25ead6ad
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-10-05 18:53:50 +0200

    the beginnings

---
+++ HelloCpp/Ul.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Ul{
+    public:
+        Ul(int startowa_liczba_przczol){
+            this->liczba_przczol = startowa_liczba_przczol;
+        }
+        bool zmiana_liczby_przczol(int liczba){
+            if(liczba >= -100 && liczba <= 100 && this->liczba_przczol + liczba >= 0){
+                this->liczba_przczol += liczba;
+                return true;
+            }
+            return false;
+        }
+        int get_liczba_przczol(){
+            return this->liczba_przczol;
+        }
+
+    private:
+        int liczba_przczol;
+        int x;
+        int y;
+        string nazwa; 
+
+};
+
+
+int main(){
+    Ul ul1(123);
+    cout<<ul1.get_liczba_przczol();
+    if(ul1.zmiana_liczby_przczol(10)){
+        cout<<ul1.get_liczba_przczol()<<endl;
+    }
+    else{
+    cout<<"zla wartosc\n";
+    }
+}
---
+++ HelloCpp/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello\n";
+    for(int i=0; i<4; i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+    }
+}
+
