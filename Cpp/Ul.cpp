abc24f330724e617c798b5a3bfce74a5128d9946
Konrad <konrad.romanski4@gmail.com>
2020-10-07 13:13:49 +0200

    structure update

--- Cpp/Ul.cpp
+++ Cpp/Ul.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class Ul{
+class Ul
+{
     public:
-        Ul(int startowa_liczba_przczol){
+        Ul(int startowa_liczba_przczol)
+        {
             this->liczba_przczol = startowa_liczba_przczol;
         }
-        bool zmiana_liczby_przczol(int liczba){
-            if(liczba >= -100 && liczba <= 100 && this->liczba_przczol + liczba >= 0){
+        bool zmiana_liczby_przczol(int liczba)
+        {
+            if(liczba >= -100 && liczba <= 100 && this->liczba_przczol + liczba >= 0)
+            {
                 this->liczba_przczol += liczba;
                 return true;
             }
             return false;
         }
-        int get_liczba_przczol(){
+        int get_liczba_przczol()
+        {
             return this->liczba_przczol;
         }
 
@@ -21,7 +26,8 @@ class Ul{
         int liczba_przczol;
         int x;
         int y;
-        string nazwa; 
+        int sredni_roczny_wskaznik_ilosci_produkowanego_miodu;
+        string nazwa_pasieki;
 
 };
 
@@ -29,10 +35,12 @@ class Ul{
 int main(){
     Ul ul1(123);
     cout<<ul1.get_liczba_przczol();
-    if(ul1.zmiana_liczby_przczol(10)){
+    if(ul1.zmiana_liczby_przczol(10))
+    {
         cout<<ul1.get_liczba_przczol()<<endl;
     }
-    else{
-    cout<<"zla wartosc\n";
+    else
+    {
+        cout<<"zla wartosc\n";
     }
 }
