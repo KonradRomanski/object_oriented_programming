9ed784ecdc61df12b5cdc44052d1fab0c9e079ba
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-10-10 22:28:38 +0200

    instances added

--- Java/Elektrownia/Elektrownia.java
+++ Java/Elektrownia/Elektrownia.java
@@ -2,5 +2,11 @@ package Elektrownia;
 
 public class Elektrownia
 {
+    private double maks_ilosc_wegla;
+    private double stan_magazynowy;
+    private double sprawnosc_elektrowni;
+    private int liczba_pracownikow;
+    private double czas_dzialania;
+
     
 }
