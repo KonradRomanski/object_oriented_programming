8373907c58842757435b8efea3ca943743bd5677
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-10-11 20:42:56 +0200

    more methods

--- Java/Elektrownia/Elektrownia.java
+++ Java/Elektrownia/Elektrownia.java
@@ -8,5 +8,25 @@ public class Elektrownia
     private int liczba_pracownikow;
     private double czas_dzialania;
 
-    
+    public Elektrownia(double nowa_maks_ilosc_wegla)
+    {
+        this.maks_ilosc_wegla = nowa_maks_ilosc_wegla;
+        this.stan_magazynowy = 0;
+        this.sprawnosc_elektrowni = 0;
+        this.liczba_pracownikow = 0;
+        this.czas_dzialania = 0;
+    }
+
+    public double odczyt_maks_ilosc_wegla()
+    {
+        return this.maks_ilosc_wegla;
+    }
+
+    public void nowy_stan_magazynowy(double nowy_stan)
+    {
+        this.stan_magazynowy = nowy_stan;
+    }
+
+    public 
+
 }
