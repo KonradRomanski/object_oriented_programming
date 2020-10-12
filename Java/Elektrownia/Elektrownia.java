d9ed278d93ebaf9be125dbffa9822de55b1620a9
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-10-12 13:40:31 +0200

    Elektrownia finished

--- Java/Elektrownia/Elektrownia.java
+++ Java/Elektrownia/Elektrownia.java
@@ -13,7 +13,7 @@ public class Elektrownia
         this.maks_ilosc_wegla = nowa_maks_ilosc_wegla;
         this.stan_magazynowy = 0;
         this.sprawnosc_elektrowni = 0;
-        this.liczba_pracownikow = 0;
+        this.liczba_pracownikow = 100;
         this.czas_dzialania = 0;
     }
 
@@ -60,14 +60,14 @@ public class Elektrownia
 
     public boolean liczba_odchodzacych_pracownikow(int odchodzacy)
     {
-        if (odchodzacy > 0) return false;
+        if (odchodzacy < 0 || this.liczba_pracownikow <=0) return false;
         this.liczba_pracownikow -= odchodzacy;
         return true; 
     }
 
     public boolean liczba_nowych_pracownikow(int nowi)
     {
-        if (nowi > 0) return false;
+        if (nowi < 0) return false;
         this.liczba_pracownikow += nowi;
         return true; 
     }
@@ -82,10 +82,36 @@ public class Elektrownia
         return this.czas_dzialania;
     }   
 
-}
+    public static void main(String[] args) {
+        {
+            Elektrownia nelektrownia = new Elektrownia(12);
 
-public static void main(String[] args) {
-    {
-        Elektrownia nelektrownia = new Elektrownia(12);
+            System.out.printf("Maks ilosc wegla: %f\n\n", nelektrownia.odczyt_maks_ilosc_wegla());
+
+            System.out.printf("Stan magazynowy: %f\n", nelektrownia.odczyt_stan_magazynowy());
+            nelektrownia.nowy_stan_magazynowy(888);
+            System.out.printf("Nowy stan magazynowy: %f\n\n", nelektrownia.odczyt_stan_magazynowy());
+
+            if(nelektrownia.maks_przerzucenie()) System.out.printf("Maks przerzucenie");
+            else System.out.printf("Maks przerzucenie niemozliwe");
+            System.out.printf("\n\n");
+
+            System.out.printf("Sprawnosc elektrowni: %f\n", nelektrownia.odczyt_sprawnosc_elektrowni());
+            nelektrownia.nowa_sprawnosc_elektrowni(99);
+            System.out.printf("Nowa sprawnosc elektrowni: %f\n\n", nelektrownia.odczyt_sprawnosc_elektrowni());
+
+            System.out.printf("Liczba pracownikow %d\n", nelektrownia.odczyt_liczby_pracownikow());
+            if (nelektrownia.liczba_odchodzacych_pracownikow(5)) System.out.printf("Usunieto pracownikow\n");
+            else System.out.printf("Nie usunieto pracownikow\n");
+            System.out.printf("Liczba pracownikow po odejsciu %d\n", nelektrownia.odczyt_liczby_pracownikow());
+            if (nelektrownia.liczba_nowych_pracownikow(10)) System.out.printf("Dodano pracownikow\n");
+            else System.out.printf("Nie dodano pracownikow\n");
+            System.out.printf("Liczba pracownikow po przyjsciu %d\n", nelektrownia.odczyt_liczby_pracownikow());
+            System.out.printf("\n");
+
+            System.out.printf("Czas dzialania elektrowni: %f\n", nelektrownia.odczyt_czasu_dzialania());
+            nelektrownia.nowy_czas_dzialania(55.6);
+            System.out.printf("Nowy dzialania elektrowni: %f\n", nelektrownia.odczyt_czasu_dzialania());
+        }
     }
 }
